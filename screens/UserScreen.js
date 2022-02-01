f6b15976bfb06c91e8f586fdb7d382fac035c08e
Marcin <marcinmaj12@gmail.com>
2022-02-01 23:45:43 +0100

    obsługa wylogowania

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,18 +1,47 @@
+import { Box, Button } from 'native-base'
+import { colors } from '../api/styles'
 import React from 'react'
 import { Text, View } from 'react-native'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import SignPanel from './SignPanel'
+import { signOut } from '../app/slices/apiSlice'
+
+
+
 
 const UserScreen = () => {
+  const dispatch = useDispatch();
 
+  const logOut = () => {
+    dispatch(signOut({}))
+  }
   const isLogged = useSelector(s => s.api.isLogged)
 
-  if(isLogged)
+  if (isLogged)
     return (
       <View>
-        <Text>
-          Dane o użytkowniku
-        </Text>
+        <Box style={{
+          flex: 1,
+          flexDirection: "row",
+          alignContent: "center",
+          justifyContent: "center"
+        }
+        }>
+          <Button style={{
+            height: 55,
+            marginTop: 20,
+            backgroundColor: colors().backgroundColor
+          }}
+            onPress={() => logOut()}
+          >
+            <Text style={{
+              fontSize: 24,
+              color: colors().color
+            }}>
+              Wyloguj
+            </Text>
+          </Button>
+        </Box>
       </View>
     )
   else
