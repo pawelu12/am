c9d8a482c5c46a83e275935a4b99d890ab70f6a0
Pukeo <95468841+Pukeo@users.noreply.github.com>
2022-02-01 12:22:06 +0100

    Zmiana HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,10 +2,25 @@ import React from 'react'
 import { SliderBox } from 'react-native-image-slider-box'
 import { colors } from '../api/styles'
 import { vw, vh } from 'react-native-css-vh-vw'
-import { Box, ScrollView, Text } from 'native-base'
+import { Box, Text } from 'native-base'
+
+const Line = ({ fontWeight, padding, paddingLeft, value, children, style }) => (
+  <Text
+    fontSize="xl"
+    style={{
+      fontWeight: fontWeight ? fontWeight : 'normal',
+      paddingRight: paddingLeft ? paddingLeft : 25,
+      padding: padding ? 15 : 0,
+      textAlign: 'right',
+      ...style
+    }}
+  >
+    { children || value }
+  </Text>
+)
 
 const HomeScreen = () => (
-  <ScrollView
+  <Box
     style={{
       width: vw(100),
       height: vh(100)
@@ -39,17 +54,19 @@ const HomeScreen = () => (
         textAlign: 'center'
       }}
     >
-      Lorem Ipsum..
+      Aplikacje Mobilne
     </Text>
-    <Text
-      fontSize="xl"
-      style={{
-        padding: 15
-      }}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Nunc sed id semper risus in hendrerit gravida rutrum. Risus feugiat in ante metus dictum at tempor commodo ullamcorper. Tempor id eu nisl nunc mi ipsum faucibus vitae. Eu facilisis sed odio morbi quis commodo. Enim lobortis scelerisque fermentum dui faucibus in ornare. Ac tortor vitae purus faucibus. Tincidunt dui ut ornare lectus. Et netus et malesuada fames ac turpis.
-    </Text>
-  </ScrollView>
+
+    <Line paddingLeft={10} style={{ textAlign: 'left', paddingLeft: 15 }}>
+      Aplikacja służy do znajdowania produktów wykonanych przez firmy, które stosują testowanie swoich wyrobów na zwierzętach. 
+    </Line>
+
+    <Line fontWeight={'bold'} paddingLeft={10} style={{ marginTop: 190 }}> Projekt został wykonany przez grupę 3IZ12A </Line>
+    <Line paddingLeft={30}> Marcin Majchrzak </Line>
+    <Line paddingLeft={30}> Damian Szabra </Line>
+    <Line paddingLeft={30}> Paweł Wikło </Line>
+    <Line paddingLeft={30}> Kacper Zieliński </Line>
+  </Box>
 )
 
 export default HomeScreen
