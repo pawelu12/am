2298e556ff587877a8f4576b36e53e31861d5d7c
kacperziel13 <99068455+kacperziel13@users.noreply.github.com>
2022-02-05 08:36:17 +0100

    Kamera
    Dodanie kamery do listy produktów

--- comps/ProductsList.js
+++ comps/ProductsList.js
@@ -3,11 +3,13 @@ import { Box, Text, HStack, Icon, Image, Input, VStack, Toast, Pressable } from 
 import React, { useState } from 'react'
 import { colors } from '../api/styles'
 import { useSelector } from 'react-redux'
-import { vw } from 'react-native-css-vh-vw'
-import { TouchableWithoutFeedback } from 'react-native'
+import { vw, vh } from 'react-native-css-vh-vw'
+import BarcodeCamera from './BarcodeCamera'
 
 const ProductsList = ({ navigation }) => {
   const products = useSelector(s => s.product.products)
+  const scanner = useSelector(s => s.api.scanner)
+
   const [ filterProducts, setFilterProducts] = useState(products)
   const [ filter, setFilter ] = useState('')
 
@@ -27,32 +29,39 @@ const ProductsList = ({ navigation }) => {
 
   return (
     <Box>
-      <Input
-        w={{ base: '100%', md: "30%" }}
-        InputRightElement= {<Icon as={<Ionicons name="search-outline" />} size={35} ml="3" style={{ marginRight: 15 }} />}
-        style={{ borderColor: colors().backgroundColor, fontSize: 24, paddingLeft: 15 }}
-        placeholder="Wyszukaj.."
-        onChangeText={setFilter}
-        onChange={searchProducts}
-      />
-      <VStack space={4} style={{ paddingTop: 10 }}>
-        {filterProducts.map(el =>
-          <Pressable key={`product-${el.id}`} onPress={() => { navigation.navigate('Products', { screen: 'Product' }) }}>
-            <HStack space={2} alignItems="flex-start">
-              <Image
-                size="xs"
-                source={{ uri: el.photo }}
-                alt={el.name}
-                resizeMode={"contain"}
-                style={{ marginLeft: 5}}
-              />
-              <Box>
-                <Text style={{ marginTop: el.name.length > 60 ? 0 : 10, width: vw(80) }}>{el.name}</Text>
-              </Box>
-            </HStack>
-          </Pressable>
-        )}
-      </VStack>
+      {scanner 
+      ?
+      <BarcodeCamera /> 
+      : 
+      <>
+        <Input
+          w={{ base: '100%', md: "30%" }}
+          InputRightElement= {<Icon as={<Ionicons name="search-outline" />} size={35} ml="3" style={{ marginRight: 15 }} />}
+          style={{ borderColor: colors().backgroundColor, fontSize: 24, paddingLeft: 15 }}
+          placeholder="Wyszukaj.."
+          onChangeText={setFilter}
+          onChange={searchProducts}
+        />
+        <VStack space={4} style={{ paddingTop: 10 }}>
+          {filterProducts.map(el =>
+            <Pressable key={`product-${el.id}`} onPress={() => { navigation.navigate('Products', { screen: 'Product' }) }}>
+              <HStack space={2} alignItems="flex-start">
+                <Image
+                  size="xs"
+                  source={{ uri: el.photo }}
+                  alt={el.name}
+                  resizeMode={"contain"}
+                  style={{ marginLeft: 5}}
+                />
+                <Box>
+                  <Text style={{ marginTop: el.name.length > 60 ? 0 : 10, width: vw(80) }}>{el.name}</Text>
+                </Box>
+              </HStack>
+            </Pressable>
+          )}
+        </VStack>
+      </>
+      }
     </Box>
   )
 }
