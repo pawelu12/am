32820244e1dc4191a8ef0bc4632fc3560a3fef84
kacperziel13 <99068455+kacperziel13@users.noreply.github.com>
2022-02-05 08:37:43 +0100

    Nowy screen
    screen z kamera

--- screens/ProductsScreen.js
+++ screens/ProductsScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ProductsList from '../comps/ProductsList'
 
 import { createStackNavigator } from '@react-navigation/stack'
+import BarcodeCamera from '../comps/BarcodeCamera'
 
 const Stack = createStackNavigator()
 
@@ -11,6 +12,7 @@ const ProductsScreen = ({ navigation }) => (
     initialRouteName="ProductList"
   >
     <Stack.Screen name="ProductList" component={ProductsList} />
+    <Stack.Screen name="Scanner" component={BarcodeCamera} />
   </Stack.Navigator>
 )
 
